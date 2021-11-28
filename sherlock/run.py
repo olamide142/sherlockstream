74a705631872d4deaa0db10dcad62a6680c00451
Victor <olowofesovictor@gmail.com>
2021-11-28 20:34:51 +0000

    update

--- sherlock/run.py
+++ sherlock/run.py
@@ -1,16 +1,37 @@
 import pdb
 import sys
+import subprocess
 import os
 from enum import Enum
 import random
-import logging
+from uuid import uuid4
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+
+from werkzeug.debug import tbtools
+
+
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 app.config['SECRET_KEY'] = 'secret'
+
+
+class Box():
+    
+    def __init__(self, executable=None):
+        self.id = uuid4()
+        self.executable = sys.executable
+
+
+class FileSystem(Box):
+    """ Manage all execution state datastructure """
+
+    def __init__(self, name=None):
+        super().__init__()
+        self.name = name 
 
 
 class State(Enum):
@@ -25,8 +46,7 @@ class SocketManager:
     @socketio.on('connect')
     def connect():
         print("Some one connected")
-        #while True:
-        emit('my response', {'data': f"{random.randint(100, 200)}"})
+        emit('my response', {'data': f'{random.randint(100, 200)}'})
 
     @socketio.event
     def disconnect():
@@ -61,11 +81,12 @@ class CodeInspector(StreamManager):
 @app.route("/")
 def main():
     streams = StreamManager.streams
+    # Trigger the debugger
+    a = 's'+1
     return render_template('index.html')
 
 
 
 if __name__ == '__main__':
-    manager = StreamManager(sys.argv[1])
+    manager = StreamManager('') #sys.argv[1])
     socketio.run(app, debug=True)
-    
